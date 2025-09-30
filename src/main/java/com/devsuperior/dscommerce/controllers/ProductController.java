3574aaaabaa9438fe3bf49a3981b000fff375303
Gabriel <gabrielkadaaj@yahoo.com.br>
2025-09-30 15:19:10 -0300

    git new post add

--- src/main/java/com/devsuperior/dscommerce/controllers/ProductController.java
+++ src/main/java/com/devsuperior/dscommerce/controllers/ProductController.java
@@ -1,15 +1,15 @@
 package com.devsuperior.dscommerce.controllers;
 
 import com.devsuperior.dscommerce.dto.ProductDto;
-import com.devsuperior.dscommerce.entities.Product;
-import com.devsuperior.dscommerce.repositories.ProductRepository;
 import com.devsuperior.dscommerce.services.ProductService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
+import java.net.URI;
 import java.util.List;
 import java.util.Optional;
 
@@ -21,14 +21,25 @@ public class ProductController {
     private ProductService service;
 
     @GetMapping(value = "/{id}")
-    public ProductDto findById(@PathVariable Long id) {
+    public ResponseEntity<ProductDto> findById(@PathVariable Long id) {
         ProductDto dto = service.findById(id);
-        return dto;
+        return ResponseEntity.ok(dto);
     }
 
     @GetMapping
-    public List<ProductDto> findAll() {
-        return service.findAll();
+    public ResponseEntity<Page<ProductDto>> findAll(Pageable pageable) {
+        Page<ProductDto> dto = service.findAll(pageable);
+        return ResponseEntity.ok(dto);
+
+
+    }
+
+    @PostMapping
+    public ResponseEntity<ProductDto> insert(@RequestBody ProductDto dto) {
+        dto = service.insert(dto);
+        URI uri = ServletUriComponentsBuilder.fromCurrentRequest().path("/{id}")
+                .buildAndExpand(dto.getId()).toUri();
+        return ResponseEntity.created(uri).body(dto);
 
     }
 }
--- src/main/java/com/devsuperior/dscommerce/services/ProductService.java
+++ src/main/java/com/devsuperior/dscommerce/services/ProductService.java
@@ -4,6 +4,8 @@ import com.devsuperior.dscommerce.dto.ProductDto;
 import com.devsuperior.dscommerce.entities.Product;
 import com.devsuperior.dscommerce.repositories.ProductRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -26,10 +28,23 @@ public class ProductService {
     }
 
     @Transactional(readOnly = true)
-    public List<ProductDto> findAll(){
-        List<Product> result = repository.findAll();
-        return result.stream().map(x -> new ProductDto(x)).toList();
+    public Page<ProductDto> findAll(Pageable pageable){
+        Page<Product> result = repository.findAll(pageable);
+        return result.map(x -> new ProductDto(x));
     }
 
+    @Transactional
+    public ProductDto insert(ProductDto dto) {
+
+        Product entity = new Product();
+        entity.setName(dto.getName());
+        entity.setDescription(dto.getDescription());
+        entity.setPrice(dto.getPrice());
+        entity.setImgUrl(dto.getImgUrl());
+
+        entity = repository.save(entity);
+
+        return new ProductDto(entity);
+    }
 
 }
