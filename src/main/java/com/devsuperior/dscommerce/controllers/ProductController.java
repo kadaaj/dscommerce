e7487fb42740d4143b90f457a168033efbe550f9
Gabriel <gabrielkadaaj@yahoo.com.br>
2025-10-01 17:46:10 -0300

    Bean Validation

--- src/main/java/com/devsuperior/dscommerce/controllers/ProductController.java
+++ src/main/java/com/devsuperior/dscommerce/controllers/ProductController.java
@@ -2,6 +2,7 @@ package com.devsuperior.dscommerce.controllers;
 
 import com.devsuperior.dscommerce.dto.ProductDto;
 import com.devsuperior.dscommerce.services.ProductService;
+import jakarta.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
@@ -33,7 +34,7 @@ public class ProductController
     }
 
     @PostMapping
-    public ResponseEntity<ProductDto> insert(@RequestBody ProductDto dto)
+    public ResponseEntity<ProductDto> insert(@Valid @RequestBody ProductDto dto)
     {
         dto = service.insert(dto);
         URI uri = ServletUriComponentsBuilder.fromCurrentRequest().path("/{id}")
@@ -42,7 +43,8 @@ public class ProductController
     }
 
     @PutMapping(value = "/{id}")
-    public ResponseEntity<ProductDto> update(@PathVariable Long id, @RequestBody ProductDto dto)
+    public ResponseEntity<ProductDto> update(@PathVariable Long id,
+                                             @Valid @RequestBody ProductDto dto)
     {
         dto = service.update(id, dto);
         return ResponseEntity.ok(dto);
